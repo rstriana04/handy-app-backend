caf82f3bf8c532f23d8df1e02ae953f0f22437cc
Robert Triana <roberttriana@esvyda.com>
2021-04-21 20:24:32 -0500

    :sparkles: Firts commit

---
+++ src/main/java/com/handy/app/handyappbackend/HandyAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.handy.app.handyappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandyAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/handy/app/handyappbackend/HandyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handy.app.handyappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
